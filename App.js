80ae7150a140baf36081a9902e0df29a77bf3b24
anitayawson <56594614+anitayawson@users.noreply.github.com>
2020-05-04 16:36:00 +0000

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, FlatList } from 'react-native';
+import SignupScreen from './SignupScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <SignupScreen/>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
